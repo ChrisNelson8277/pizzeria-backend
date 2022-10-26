11c6ba62f975ac5e097f8b7cf9a7e1526fe3c948
Chris Nelson <ChrisNelson8277@users.noreply.github.com>
2022-10-26 00:21:17 -0400

    test

--- server.js
+++ server.js
@@ -34,9 +34,7 @@ app.get("/success", async (req, res) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
   const customer = await stripe.customers.retrieve(session.customer);
 
-  res.send(
-    `<html><body><h1>Thanks for your order, ${customer.name}!</h1></body></html>`
-  );
+  res.send("is this working");
 });
 
 app.post("/db/addMenuItems", (req, res) => {
